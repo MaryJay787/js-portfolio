bdac42cdcea0b2ae9a98211926669c7da0e3119a
MaryJay787 <josephmaryam.mj@gmail.com>
2022-08-07 19:35:26 +0000

    error with btn transitions

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelectorAll('.main-content');
 
+
 function PageTurner(){
     //Button click active class
      for(let i=0; i < sectBtn.length; i++){
-         sectBtn[i].addEventListener('click', () =>{
+         sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += 'active-btn'
+            this.className += 'active-btn';
         })
      }
 
